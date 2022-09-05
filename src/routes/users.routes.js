37cb047ea53926a8489a0bc9d21e2dcdadea04b3
loetitia <audiger.loetitia@gmail.com>
2022-09-05 10:44:47 +0200

    ajout route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -12,13 +12,13 @@ const router = express.Router();
 //Auth
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", logout);
+router.get("/logout", logout);
 
 //User
 
 router.get("/", getUsers);
 router.get("/:id", getUser);
-router.get("/:id", updateUser);
-router.get("/:id", deleteUser);
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 export default router;
